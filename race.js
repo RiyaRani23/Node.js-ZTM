3900fdf52526ff5075a3f685fa968d0b0e1c8693
RiyaRani23 <riyarani1404@gmail.com>
2025-09-01 20:01:51 +0530

    Asynchronous call

---
+++ race.js
@@ -0,0 +1,10 @@
+// Synchronous call
+
+console.log('🐇 finishes !! ');
+console.log('🐢 finishes !! ');
+
+
+// Asynchronous call 
+
+setTimeout(() => console.log('🐇 finishes !! ') , 1000);
+console.log('🐢 finishes !! ');
