dd4867ce435d9367aec674de108083092c3a85b7
RiyaRani23 <riyarani1404@gmail.com>
2025-09-07 17:07:01 +0530

    Creating First Web Server

--- P07_HTTP-Server/index.js
+++ P07_HTTP-Server/index.js
@@ -4,9 +4,13 @@ const PORT = 3000;
 
 const server = http.createServer((req , res) => {
     res.writeHead(200, {
-        'Content-Type' : 'text/plain',
+        // 'Content-Type' : 'text/plain',
+          'Content-Type' : 'application/json',
     });
-    res.end('Hello!! Sir Issac Newton is your friend');
+    res.end(JSON.stringify ({
+        id : 1 ,
+        name : 'Sir Issac Newton',
+    }));
 });
 
 server.listen(PORT , () => {
