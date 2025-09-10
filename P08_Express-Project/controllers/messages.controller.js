97fd6249265999ccfdf6e975d3c790fe840bde8d
RiyaRani23 <riyarani1404@gmail.com>
2025-09-10 21:31:58 +0530

    Express Project

--- P08_Express-Project/controllers/messages.controller.js
+++ P08_Express-Project/controllers/messages.controller.js
@@ -3,7 +3,7 @@ const path = require('path'); // /folder/files.jpg \folder\files.jpg
 
 // function getMessages (req , res)   {
 // image upload
-//    res.sendFile(path.join(_dirname, '..', 'public', 'skimountain.jpg'));
+//    res.sendFile(path.join(_dirname, '..', 'public', 'images' , 'skimountain.jpg'));
 //    res.send('<ul><li> Hello Albert! </li></ul>');
 // }
 
