9bcd70316ee4856058f83f7a7378842da88f0745
RiyaRani23 <riyarani1404@gmail.com>
2025-09-13 13:19:15 +0530

    Planets Model

--- P09_NASA-Project/server/src/models/planets.model.js
+++ P09_NASA-Project/server/src/models/planets.model.js
@@ -1,3 +1,60 @@
-const planets = [];
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
-module.exports = planets;
+// const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] ==='CONFIRMED'
+  && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6;
+}
+
+/* const promise = new Promise ((resolve , reject) => {
+    resolve(42);
+    })
+    promise.then (result) => {
+        };
+    const result = await promise; 
+    console.log(result);
+    */
+
+function loadPlanetData(){
+    return new Promise((resolve , reject) => {
+         fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+       .pipe(parse ({
+          comment : '#',
+          columns : true,
+        }))
+       .on('data' , (data) => {
+        if(isHabitablePlanet(data)){
+          habitablePlanets.push(data);
+       }
+    // results.push(data);
+  })
+  .on ('error' , (err) =>{
+    console.log(err);
+    reject(err);
+  })
+  .on('end' , () => {
+    //  console.log(results);
+    // console.log('done');
+
+    //  console.log(habitablePlanets.map((planet) => {
+    //       return planet['kepler_name'];
+    //  }));
+
+      console.log(`${habitablePlanets.length} habitable planets found !!`);
+      resolve();
+  });
+});
+}
+
+
+//   parse();
+
+module.exports = {
+    loadPlanetData,
+    planets : habitablePlanets, 
+};
--- P09_NASA-Project/server/src/server.js
+++ P09_NASA-Project/server/src/server.js
@@ -2,13 +2,21 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app); 
+
+async function startServer (){
+    await loadPlanetData();
+}
+
 
 server.listen(PORT,() =>{
     console.log(`Listening on port ${PORT}...`);
 })
 
 
- console.log(PORT);
+//  console.log(PORT);
+
+startServer();
